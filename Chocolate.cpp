c5cad85bd67318ebc85bc8eae44cd23622b88f3b
Unknown <ttrinkle@ucmerced.edu>
2017-05-08 14:36:31 -0700

    Clarification in comments in the event functions

--- Chocolate.cpp
+++ Chocolate.cpp
@@ -4,5 +4,8 @@ Chocolate::Chocolate(float x, float y, float w, float h) : Block(x, y, w, h) { }
 
 void Chocolate::event(bool top)
 {
-     //doggo->die();
+     // Not sure how to handle events yet, but Chocolate
+     // objects should not care about where the Objects
+     // collide, it should seek to end the game upon any
+     // instance of collision
 }
--- Trampoline.cpp
+++ Trampoline.cpp
@@ -5,6 +5,8 @@ Trampoline::Trampoline(float x, float y, float w, float h) : Block(x, y, w, h) {
 void Trampoline::event(bool top)
 {
      if(top){
-          //doggo->isJumping = 1;
+          //Not sure how to handle events, want it 
+          // To only register when the object is on top
+          // of the Trampoline, as denoted by the bool
      }
 }
